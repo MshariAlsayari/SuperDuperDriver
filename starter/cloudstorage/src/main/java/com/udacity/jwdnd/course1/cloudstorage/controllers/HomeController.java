45c97106f4cb08f219b404d716e6c7d652c21fc9
mshariQurrah <83126503+mshariQurrah@users.noreply.github.com>
2021-10-05 21:02:51 +0300

    - implement adding and displaying credentials

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,18 +1,20 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 
+import com.udacity.jwdnd.course1.cloudstorage.models.Credential;
 import com.udacity.jwdnd.course1.cloudstorage.models.File;
 import com.udacity.jwdnd.course1.cloudstorage.models.Note;
 import com.udacity.jwdnd.course1.cloudstorage.models.User;
-import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
-import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
-import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import com.udacity.jwdnd.course1.cloudstorage.services.*;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.security.SecureRandom;
+import java.util.Base64;
 
 @Controller
 @RequestMapping("/home")
@@ -22,17 +24,22 @@ public class HomeController {
     private final FileService fileService;
     private final UserService userService;
     private final NoteService noteService;
+    private final CredentialService credentialService;
+    private final EncryptionService encryptionService;
 
-    public HomeController(FileService fileService, UserService userService, NoteService noteService) {
+    public HomeController(FileService fileService, UserService userService, NoteService noteService, CredentialService credentialService, EncryptionService encryptionService) {
         this.fileService = fileService;
         this.userService = userService;
         this.noteService = noteService;
+        this.credentialService = credentialService;
+        this.encryptionService = encryptionService;
     }
 
     @GetMapping
     public String homeView(Model model) {
         model.addAttribute("files", this.fileService.getFiles());
         model.addAttribute("notes", this.noteService.getAllNotes());
+        model.addAttribute("credentials", this.credentialService.getAllCredential());
         return "home";
     }
 
@@ -116,6 +123,26 @@ public class HomeController {
     public String deleteNote(@PathVariable(name = "noteId") Integer noteId, Model model) {
         noteService.deleteNote(noteId);
         model.addAttribute("notes", this.noteService.getAllNotes());
+        return "home";
+    }
+
+    @PostMapping("credentials")
+    public String submitCredential(Authentication authentication, @ModelAttribute Credential credential, Model model) {
+
+        User user = userService.getUser(authentication.getName());
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        random.nextBytes(key);
+        String encodedKey = Base64.getEncoder().encodeToString(key);
+        String encryptedPassword = encryptionService.encryptValue(credential.getPassword(), encodedKey);
+        credential.setUserid(user.getUserId());
+        credential.setKey(encodedKey);
+        credential.setPassword(encryptedPassword);
+
+        credentialService.createCredential(credential);
+
+
+        model.addAttribute("credentials", this.credentialService.getAllCredential());
         return "home";
     }
 
---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/CredentialMapper.java
@@ -0,0 +1,26 @@
+package com.udacity.jwdnd.course1.cloudstorage.mappers;
+
+import com.udacity.jwdnd.course1.cloudstorage.models.Credential;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CredentialMapper {
+
+    @Select("Select * from CREDENTIALS WHERE credentialid = #{credentialId}")
+    Credential getCredential(Integer credentialId);
+
+    @Select("Select * from CREDENTIALS")
+    List<Credential> getAllCredentials();
+
+    @Delete("DELETE FROM CREDENTIALS WHERE credentialid = #{credentialId}")
+    void deleteCredential(Integer credentialId);
+
+    @Update("UPDATE CREDENTIALS SET url = #{url},username = #{username}, password = #{password} WHERE credentialid = #{credentialId}")
+    void updateNote(Integer credentialId, String url, String username, String password);
+
+    @Insert("INSERT INTO CREDENTIALS (url, username, key, password, userid) VALUES(#{url}, #{username}, #{key}, #{password}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialId")
+    int insertCredential(Credential credential);
+}
